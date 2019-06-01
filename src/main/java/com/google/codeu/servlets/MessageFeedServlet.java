9fd39d34dd03f6115fc54038c6fba3202debfae1
LISH0038 <melodyli710@gmail.com>
2019-06-01 14:37:57 +0800

    add data store logic: add getAllMessages function

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -1,4 +1,22 @@
 package com.google.codeu.servlets;
 
-public class MessageFeedServlet {
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Handles fetching all messages for the public feed.
+ */
+@WebServlet("/feed")
+public class MessageFeedServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        response.getOutputStream().println("this will be my message feed");
+    }
 }
